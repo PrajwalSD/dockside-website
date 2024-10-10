6e0817e2d4d2ac3fb0672602c4df4795916c0fd6
Prajwal Shetty <hachipoo.apps@gmail.com>
2024-10-11 00:45:43 +0100

    First draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import typographyPlugin from '@tailwindcss/typography';
 
-export default {
+module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
